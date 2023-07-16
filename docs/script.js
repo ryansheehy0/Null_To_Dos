0dcaa5c8e6c1f6264131cef5851c13084ef1a462
Ryan Sheehy <ryansheehy0@gmail.com>
2023-07-15 22:03:47 -0700

    Resizing name textareas.

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,22 @@
 // Todo
-  // Local storage for lists
+  // Delete button
   // Autofocus when creating new list/card
-  // Enter key to remove focus from input
-  // Input can have line wrap
-  // Input gets resized to the buttons
-  // Stack cards
+  // Local storage for lists
+  // Enter to create new list like trello
+    // Disable default behavior of enter key for textarea. Shift enter to create new line.
+    // Enter key presses the parent's add/plus button. Need autofocus done before this feature
+  // Drag cards around
+  // Make logo and maybe rename
+// Paid options. Cheaper than $5 a month
+  // Work with other people like google docs
+  // Can have multiple different pages
+  // Can change the theme and other settings
 
 let addListBtn = document.querySelector("#add-list-btn")
 addListBtn.addEventListener("click", () => {
   addListBtn.insertAdjacentHTML("beforebegin", `
     <div class="list">
-      <input type="text" class="name"></input>
+      <textarea class="name" oninput="autoResize(this)" rows="1"></textarea>
       <div class="buttons">
         <i class="bi bi-plus plus-icon"></i>
         <i class="bi bi-trash trash-icon"></i>
@@ -24,7 +30,7 @@ document.querySelector("body").addEventListener("click", (event) => {
   if(event.target.className.includes("plus-icon") && event.target.parentNode.className.includes("buttons")){
     event.target.parentNode.parentNode.insertAdjacentHTML("beforeend", `
       <div class="card">
-        <input type="text" class="name"></input>
+        <textarea class="name" oninput="autoResize(this)" rows="1"></textarea>
         <div class="buttons">
           <i class="bi bi-plus plus-icon"></i>
           <i class="bi bi-trash trash-icon"></i>
@@ -37,3 +43,8 @@ document.querySelector("body").addEventListener("click", (event) => {
     console.log("trash-icon")
   }
 })
+
+function autoResize(textarea) {
+  textarea.style.height = "fit-content";
+  textarea.style.height = textarea.scrollHeight + "px"
+}
