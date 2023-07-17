069ab977813df1862874c7e5ed61a8a9c639c923
Ryan Sheehy <ryansheehy0@gmail.com>
2023-07-17 15:05:12 -0700

    Adds local storage.

--- docs/script.js
+++ docs/script.js
@@ -1,21 +1,29 @@
 // Todo
-  // Local storage for lists
-  // Enter to create new list like trello
-    // Disable default behavior of enter key for textarea. Shift enter to create new line.
-    // Enter key presses the parent's add/plus button. Need autofocus done before this feature
-  // Drag cards around
-  // Make logo and maybe rename
 // Paid options. Cheaper than $5 a month
   // Work with other people like google docs
   // Can have multiple different pages
   // Can change the theme and other settings
+    // Toggle spell checking
+// Possible features
+  // Drag cards around
+  // Enter to create new list like trello
+    // Disable default behavior of enter key for textarea. Shift enter to create new line.
+    // Enter key presses the parent's add/plus button. Need autofocus done before this feature
+  // Make logo and maybe rename
+
+let bodyHTML = localStorage.getItem("body")
+if(bodyHTML === null){
+  listsAndCards = ""
+}else{
+  document.body.innerHTML = bodyHTML
+}
 
 // Add new list
 let addListBtn = document.querySelector("#add-list-btn")
 addListBtn.addEventListener("click", () => {
   addListBtn.insertAdjacentHTML("beforebegin", `
     <div class="list">
-      <textarea class="name" oninput="autoResize(this)" rows="1"></textarea>
+      <textarea class="name" oninput="autoResize(this)" rows="1" spellcheck="false"></textarea>
       <div class="buttons">
         <i class="bi bi-plus plus-icon"></i>
         <i class="bi bi-trash trash-icon"></i>
@@ -23,6 +31,8 @@ addListBtn.addEventListener("click", () => {
       <!-- More cards -->
     </div>
   `)
+  localStorage.setItem("body", document.body.innerHTML)
+  // Set focus
   let lists = document.querySelectorAll(".name")
   lists[lists.length - 1].focus()
 })
@@ -38,7 +48,7 @@ document.body.addEventListener("click", (event) => {
     let parentListOrCard = event.target.parentNode.parentNode
     parentListOrCard.insertAdjacentHTML("beforeend", `
       <div class="card">
-        <textarea class="name" oninput="autoResize(this)" rows="1"></textarea>
+        <textarea class="name" oninput="autoResize(this)" rows="1" spellcheck="false"></textarea>
         <div class="buttons">
           <i class="bi bi-plus plus-icon"></i>
           <i class="bi bi-trash trash-icon"></i>
@@ -47,6 +57,7 @@ document.body.addEventListener("click", (event) => {
       </div>
     `)
     parentListOrCard.children[parentListOrCard.children.length - 1].querySelector(".name").focus()
+    localStorage.setItem("body", document.body.innerHTML)
   }
   // Delete button
   if(event.target.className.includes("trash-icon")){
@@ -57,6 +68,7 @@ document.body.addEventListener("click", (event) => {
       event.target.parentNode.parentNode.remove()
       modal.style.visibility = "hidden";
       bodyContainer.style.pointerEvents = "auto" // Allow clicking the background
+      localStorage.setItem("body", document.body.innerHTML)
     }
     ok.addEventListener("click", okEventListener)
     // Cancel is pressed
@@ -72,4 +84,6 @@ document.body.addEventListener("click", (event) => {
 function autoResize(textarea) {
   textarea.style.height = "fit-content";
   textarea.style.height = textarea.scrollHeight + "px"
+  textarea.textContent = textarea.value
+  localStorage.setItem("body", document.body.innerHTML)
 }
