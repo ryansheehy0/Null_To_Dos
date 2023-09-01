014398976668013d6bbbcdddef7eb72c9c00d4ef
Ryan Sheehy <ryansheehy0@gmail.com>
2023-09-01 15:57:28 -0700

    Changed name and added favicon.

--- Develop/server.js
+++ Develop/server.js
@@ -4,5 +4,5 @@ const app = express()
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3001, () =>{
-  console.log(`Listening on port ${process.env.PORT}.`)
+  console.log(`Server is listening.`)
 })
