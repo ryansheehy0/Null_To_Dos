b5a1d525c6265b0f10e77293c24595e261f79e1c
Ryan Sheehy <ryansheehy0@gmail.com>
2023-09-07 11:49:12 -0700

    Changes focus on textareas so it changes with mode and adds red trash icon instead of delete model.

--- Develop/public/component_classes/board.js
+++ Develop/public/component_classes/board.js
@@ -5,7 +5,6 @@ function Board(){
 
   let element = window.elementFromHTML(`
     <div data-name="board" class="w-screen h-screen grid grid-flow-col overflow-x-auto justify-start">
-
       <div id="add-list-btn" class="cursor-pointer flex items-center justify-center custom-text-color custom-2nd-color w-64 min-h-8 py-1.5 px-3 mx-1 my-2 rounded-xl h-fit box-content">
         <img src="./assets/plus.svg" class="custom-img-color w-8 h-8">
         Add another list
@@ -16,21 +15,10 @@ function Board(){
   this.getElement = function(){return element}
 
   this.addList = function(list){
-    // Add list to lists
-    lists.push(list)
     // Get add-list-btn inside board
     const addListBtn = element.querySelector(`#add-list-btn`)
     // Put list inside board
     addListBtn.insertAdjacentElement(`beforebegin` /*Above*/, list.getElement())
-  }
-
-  this.removeList = function(list){
-    // Remove list from lists
-    lists = lists.filter(item => {
-      if(item === list) return false
-    })
-    // Remove list form board
-    element.removeChild(list)
   }
 
   this.load = function(){
--- Develop/public/component_classes/list.js
+++ Develop/public/component_classes/list.js
@@ -1,40 +1,46 @@
 function List(){
-  let cards = []
   // Draggable events
 
   let uuid = window.getNewUUID()
 
   let element = window.elementFromHTML(`
-    <div data-name="list" class="custom-text-color rounded-xl py-1.5 px-3 min-w-[16rem] w-fit grid grid-cols-[auto_auto] min-h-8 h-fit mx-1 my-2 custom-2nd-color box-content" draggable="true" data-uuid="${uuid}">
-      <textarea data-name="name" class="m-0 flex items-center border-none bg-transparent custom-text-color text-base h-fit resize-none mt-auto mb-auto focus:outline-none" oninput="window.resizeTextarea(this)" rows="1" spellcheck="false"></textarea>
+    <div data-name="list" class="custom-text-color rounded-xl py-1.5 px-3 min-w-[16rem] w-fit grid grid-cols-[auto_auto] min-h-8 h-fit mx-1 my-2 custom-2nd-color box-content" data-uuid="${uuid}">
+      <textarea class="m-0 flex items-center border-none bg-transparent custom-text-color text-base h-auto resize-none mt-auto mb-auto pl-1 focus:rounded focus:outline focus:outline-1 focus:custom-text-outline" oninput='this.style.height = this.scrollHeight + "px"' rows="1" spellcheck="false"></textarea>
       <div data-name="buttons" class="flex items-center justify-end">
         <img src="./assets/plus.svg" class="custom-img-color w-8 h-8">
         <img src="./assets/trash.svg" class="custom-img-color w-5 h-5">
       </div>
-      <!-- More cards -->
     </div>
   `)
 
-/*
-.name:focus{
-  border-radius: 5px;
-  outline: none;
-  outline: 1px solid var(--light);
-}
-*/
+  let plus = element.querySelector(`img[src*="plus"]`)
+  let trash = element.querySelector(`img[src*="trash"]`)
+
+  plus.addEventListener("click", () => {
+  })
+
+  trash.addEventListener("click", () => {
+    // If it is already red remove the list
+    if([...trash.classList].includes('custom-red-color')){
+      element.remove()
+      return
+    }
+    // Change trash to red
+    trash.classList.add('custom-red-color')
+  })
+
+  document.addEventListener("click", (event) => {
+    if(event.target !== trash){
+      if([...trash.classList].includes('custom-red-color')){
+        trash.classList.remove('custom-red-color')
+      }
+    }
+  })
 
   this.getElement = function(){return element}
 
   this.addCard = function(card){
-    cards.push(card)
-  }
-
-  this.removeCard = function(id){
-    cards = cards.filter(card => {
-      if(card.id === id){
-        return false
-      }
-    })
+    element.insertAdjacentElement(`beforeend` /*Last Child*/, card.getElement())
   }
 }
 
